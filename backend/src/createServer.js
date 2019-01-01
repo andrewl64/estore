fc7712687735d99403b89db9c648a2956425a0a4
unknown <shillongtitude@gmail.com>
2019-01-02 04:11:55 +0530

    commit for setupof connection between database and queries/mutations

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,8 +1,9 @@
-const { GraphQLServer } = require("graphql-yoga");
-
+const { GraphQLServer } = require('graphql-yoga');
 const Mutation = require('./resolvers/Mutation');
 const Query = require('./resolvers/Query');
-const db =  require('./db');
+const db = require('./db');
+
+
 
 function createServer() {
 	return new GraphQLServer({
@@ -12,7 +13,7 @@ function createServer() {
 			Query
 		},
 		resolverValidationOptions: {
-			requireResolversForResolveType: false
+			requireResolversForResolveType: false,
 		},
 		context: req => ({ ...req, db }),
 	});
